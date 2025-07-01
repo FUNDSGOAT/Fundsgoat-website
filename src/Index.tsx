3d070eb38a208cfe44b843a5a595a5d8c11f4210
FUNDSGOAT <fundsgoatapprovalservices@gmail.com>
2025-06-30 22:32:15 -0400

    Create Index.tsx

---
+++ src/Index.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      
+      <main>
+        {/* All your other content like hero section, consultation, etc. */}
+      </main>
+    </>
+  );
+};
+
+export default Index;
