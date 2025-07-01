c23e9c0d76ffe2fae38f2e75d1c07fe8d77a8da0
FUNDSGOAT <fundsgoatapprovalservices@gmail.com>
2025-06-30 21:47:06 -0400

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import ConsultationForm from "../components/ConsultationForm";
+
+const Index = () => {
+  return (
+    <main>
+      <ConsultationForm />
+    </main>
+  );
+};
+
+export default Index;
